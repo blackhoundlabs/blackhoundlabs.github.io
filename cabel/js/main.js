e15db168946de5ece3d7539058a26dc3c2c2c8d7
blackhoundlabs <black.hound.labs@gmail.com>
2017-11-02 18:17:14 +0300

    Add files via upload

---
+++ cabel/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor : ['transparent', 'transparent', 'transparent', 'transparent', '#f16529'],
+		loopHorizontal: true,
+		// controlArrows: true,
+		// menu: '#menu',
+		anchors: ['header', 'metall', 'cable', 'contact', 'footer'],
+	});
+});

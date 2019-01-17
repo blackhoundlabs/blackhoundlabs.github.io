0e80105b6698e99f0943bfef6ec56281322e8310
mike dex <m.mitroshkin@ren-tv.com>
2019-01-17 13:45:56 +0300

    remove cabel

--- cabel/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$('#fullpage').fullpage({
-		sectionsColor : ['transparent', 'transparent', 'transparent', 'transparent', '#f16529'],
-		loopHorizontal: true,
-		// controlArrows: true,
-		// menu: '#menu',
-		anchors: ['header', 'metall', 'cable', 'contact', 'footer'],
-	});
-});
